112a0f65e56cb53b7be6d39864e2fd5f8f8e9ee1
tblivet <thibault.blivet@prestashop.com>
2024-08-12 18:03:02 +0200

    chore: lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "@prestashopcorp/stylelint-config",
   "rules": {
     "comment-empty-line-before": null,
+    "no-unknown-animations": null,
     "scss/at-import-no-partial-leading-underscore": null,
   }
 };
