41eb272168d78c8871225f09d081346359c754a9
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-10-14 09:04:28 +0200

    fix: archive prestashop renamed to archive zip

--- .github/action_upgrade.sh
+++ .github/action_upgrade.sh
@@ -25,7 +25,7 @@ if [[ $CHANNEL == "local" ]]; then
   docker exec -u www-data prestashop_autoupgrade mkdir admin-dev/autoupgrade/download
   docker exec -u www-data prestashop_autoupgrade curl -L $ARCHIVE_URL -o admin-dev/autoupgrade/download/prestashop.zip
   docker exec -u www-data prestashop_autoupgrade curl -L $XML_URL -o modules/autoupgrade/download/prestashop.xml
-  echo "{\"channel\":\"local\",\"archive_prestashop\":\"prestashop.zip\",\"archive_num\":\"${VERSION}\", \"archive_xml\":\"prestashop.xml\", \"PS_AUTOUP_CHANGE_DEFAULT_THEME\":${UPDATE_THEME}, \"skip_backup\": ${SKIP_BACKUP}}" > modules/autoupgrade/config.json
+  echo "{\"channel\":\"local\",\"archive_zip\":\"prestashop.zip\",\"archive_num\":\"${VERSION}\", \"archive_xml\":\"prestashop.xml\", \"PS_AUTOUP_CHANGE_DEFAULT_THEME\":${UPDATE_THEME}, \"skip_backup\": ${SKIP_BACKUP}}" > modules/autoupgrade/config.json
   docker exec -u www-data prestashop_autoupgrade php modules/autoupgrade/bin/console update:start --action="compareReleases" --config-file-path="modules/autoupgrade/config.json" admin-dev
   docker exec -u www-data prestashop_autoupgrade php modules/autoupgrade/bin/console update:start --config-file-path="modules/autoupgrade/config.json" admin-dev
 fi
